97b61aa3fa95b950f8dfb94bc6e262a9e8247914
diksha <me.dishasharma7398@gmail.com>
2021-06-28 20:33:42 +0530

    changes for customer query......

--- beverage.js
+++ beverage.js
@@ -1,29 +1,68 @@
-const fs = require ("fs");
+const fs = require("fs");
 
-const getRecords = () =>{
-    const recordedData = fs.readFileSync("sales.json","utf-8");
-    return recordedData.length ? JSON.parse(recordedData) : [];
+const getRecords = () => {
+  const recordedData = fs.readFileSync("sales.json", "utf-8");
+  return recordedData.length ? JSON.parse(recordedData) : [];
 };
 
 const writeRecords = function (recordToAdd) {
-    fs.writeFileSync("sales.json",recordToAdd);
+  fs.writeFileSync("sales.json", recordToAdd);
 };
 
 const updateRecords = function (newEntry) {
-    const feededRecords = getRecords();
-    feededRecords.push(newEntry);
-    return JSON.stringify(feededRecords);
+  const feededRecords = getRecords();
+  feededRecords.push(newEntry);
+  return JSON.stringify(feededRecords);
+};
+
+const date = new Date();
+const [input,firstOpt, beverage ,secondOpt, qty, thirdOpt, empId,FourthOpt,inputDate] = process.argv.slice(2);
+if (input=="--save")
+{
+    if (
+        Number.isInteger(Number(qty)) &&
+        Number.isInteger(Number(empId)) &&
+        typeof beverage == "string"
+      ) {
+        const newRecord = [
+          { empId: empId, beverage: beverage, qty: qty, date: date },
+        ];
+        writeRecords(updateRecords(newRecord));
+      } else {
+        console.log("Oops! Invalid Input");
+      }     
+}
+else if (input =="--query")
+{
+   
+  let allRecords = getRecords();
+
+  const checkOptions = function(Option,value)
+  {
+    let list;
+   if (Option  == "--beverage")
+   {
+   allRecords= allRecords.filter(record =>record.beverage == value);
+   }
+   if (Option =="--empId")
+   {
+    allRecords= allRecords.filter(record =>record.empId== value);
+   }
+   if (Option =="--date")
+   {
+    allRecords= allRecords.filter(record =>record.date== value);
+   }
+   if (Option =="--qty")
+   {
+    allRecords= allRecords.filter(record =>record.qty== value);
+   }
+   return allRecords ;
    };
 
-const curDate = new Date();
-const [,beverage,,qty,,empId] =process.argv.slice(2);
- if (Number.isInteger(Number(qty)) && Number.isInteger(Number(empId)) && typeof(beverage)=="string")
- {
-    const newRecord = [{empId:empId,beverage:beverage,qty:qty,date : curDate}];
-    writeRecords(updateRecords(newRecord));
-}
-else
-{
-    console.log("Oops! Invalid Input")
-}
-
+   checkOptions(firstOpt,beverage);
+   checkOptions(secondOpt,qty);
+   checkOptions(thirdOpt,empId);
+   checkOptions(FourthOpt,inputDate);
+   console.log(allRecords);
+ }
+ 
