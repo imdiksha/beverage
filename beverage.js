057ede670f053d3a17b67144ba99245817d5c1d2
diksha <me.dishasharma7398@gmail.com>
2021-06-28 12:33:09 +0530

    Modified for save with validation...

--- beverage.js
+++ beverage.js
@@ -1,6 +1,29 @@
 const fs = require ("fs");
 
-const [beverage,bevval,qty,qtyval,empid,empval] =process.argv.slice(2);
-const newRecord = [beverage,bevval,qty,qtyval,empid,empval];
-fs.writeFileSync("sales.json",JSON.stringify(newRecord));
+const getRecords = () =>{
+    const recordedData = fs.readFileSync("sales.json","utf-8");
+    return recordedData.length ? JSON.parse(recordedData) : [];
+};
+
+const writeRecords = function (recordToAdd) {
+    fs.writeFileSync("sales.json",recordToAdd);
+};
+
+const updateRecords = function (newEntry) {
+    const feededRecords = getRecords();
+    feededRecords.push(newEntry);
+    return JSON.stringify(feededRecords);
+   };
+
+const curDate = new Date();
+const [,beverage,,qty,,empId] =process.argv.slice(2);
+ if (Number.isInteger(Number(qty)) && Number.isInteger(Number(empId)) && typeof(beverage)=="string")
+ {
+    const newRecord = [{empId:empId,beverage:beverage,qty:qty,date : curDate}];
+    writeRecords(updateRecords(newRecord));
+}
+else
+{
+    console.log("Oops! Invalid Input")
+}
 
